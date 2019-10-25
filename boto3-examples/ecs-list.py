c38ac182138e5c647cb4a52f64e6f10631d144b3
emanuelaguna <emanuelaguna@gmail.com>
2019-10-25 02:34:18 -0500

    list more information about instances

--- boto3-examples/ecs-list.py
+++ boto3-examples/ecs-list.py
@@ -1,13 +1,37 @@
 import boto3
+import click
 
 """
 create a list of intances from aws 
 """
 
-if __name__ == '__main__':
-    session = boto3.Session(profile_name='paco')
-    ec2 = session.resource('ec2')
+session = boto3.Session(profile_name='paco')
+ec2 = session.resource('ec2')
 
+@click.command()
+def list_instances():
+    "List EC2 Instances"
     for i in ec2.instances.all():
-        print(i)
+        print(','.join((
+            i.id,
+            i.instance_type,
+            i.placement['AvailabilityZone'],
+            i.state['name'],
+            i.public_dns_name
+        )))
+    return
 
+
+if __name__ == '__main__':
+    print(sys.argv)
+    list_instances()
+
+# user list instances
+
+# user instances create-snapshot
+
+# user list volumes
+
+# user stop instances tag=Project: <tag_value>
+
+# user start instances --project = <tag_value>
