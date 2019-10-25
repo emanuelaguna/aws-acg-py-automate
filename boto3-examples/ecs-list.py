8cb87c84586b34674051fff054f4295d56f30f91
emanuelaguna <emanuelaguna@gmail.com>
2019-10-25 13:39:38 -0500

    Handle error from AWS when trying to stopping a pendding start instance or start a pendding one

--- boto3-examples/ecs-list.py
+++ boto3-examples/ecs-list.py
@@ -1,4 +1,5 @@
 import boto3
+import botocore
 import click
 
 """
@@ -20,8 +21,82 @@ def filter_instances(project):
     return instances
 
 @click.group()
+def cli():
+    """Paco manages instnaces"""
+
+@cli.group('snapshots')
+def snapshots():
+    """commands for sbnapshots"""
+
+@snapshots.command('list')
+@click.option('--project', default=None, help="Only snapshots for project tag project:<name>")
+def list_snapshots(project):
+    "List snapshots of an Instances"
+
+    instances = filter_instances(project)
+
+    for i in instances:
+        for v in i.volumes.all():
+            for s in v.snapshots.all():
+                print(','.join((
+                    s.id,
+                    v.id,
+                    i.id,
+                    s.state,
+                    s.progress,
+                    s.start_time.strftime("%c")
+                )))
+    return
+
+@cli.group('volumes')
+def volumes():
+    """commands for volumes"""
+
+@volumes.command('list')
+@click.option('--project', default=None, help="Only instances for project tag project:<name>")
+def list_volumes(project):
+    "List volumes of an Instances"
+
+    instances = filter_instances(project)
+
+    for i in instances:
+        for v in i.volumes.all():
+            print(','.join((
+                v.id,
+                i.id,
+                str(v.size) + 'GiB',
+                v.state,
+                v.encrypted and 'Encrypted' or 'Not Encrypted'
+            )))
+    return
+
+
+@cli.group('instances')
 def instances():
-    """Commands for instnaces"""
+    """manages instances"""
+
+@instances.command('snapshot')
+@click.option('--project', default=None, help="only create snapshots of instances for tag project:<name>")
+def create_snapshot(project):
+
+    instances = filter_instances(project)
+
+    for i in instances:
+        print("Stopping ... {0}".format(i.id))
+        i.stop()
+        i.wait_until_stopped()
+
+        for v in i.volumes.all():
+            print(" Creating a snapshot of ...{0}".format(v.id))
+            v.create_snapshot(Description="Created by paco  automated bot")
+
+        print("Starting ...{0}".format(i.id))
+        i.start()
+        i.wait_until_running()
+
+    print("Jobs done!")
+    return
+
 
 @instances.command('list')
 @click.option('--project', default=None, help="Only instances for project tag project:<name>")
@@ -51,8 +126,12 @@ def stop_instances(project):
     instances = filter_instances(project)
 
     for i in instances:
-        print("Stopping instance {0} ...".format(i.id))
-        i.stop()
+        print("Stopping instance {0}...".format(i.id))
+        try:
+            i.stop()
+        except botocore.exceptions.ClientError as e:
+            print("Could not stop instance {0}. ".format(i.id) + str(e))
+            continue
 
     return
 
@@ -65,13 +144,17 @@ def stop_instances(project):
     instances = filter_instances(project)
 
     for i in instances:
-        print("Starting instance {0} ...".format(i.id))
-        i.start()
+        print("Starting instance {0}...".format(i.id))
+        try:
+            i.start()
+        except botocore.exceptions.ClientError as e:
+            print("Could not start instance {0}. ".format(i.id) + str(e))
+            continue
 
     return
 
 if __name__ == '__main__':
-    instances()
+    cli()
 
 
 # user instances create-snapshot
